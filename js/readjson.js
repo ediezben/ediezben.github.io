84954a8b59aed1c016824c0af10d697bc8f3ad6e
ediezben <ediezben@gmail.com>
2021-10-26 16:57:39 +0200

    Test9

--- js/readjson.js
+++ js/readjson.js
@@ -1,13 +1,5 @@
 function load() {
-  var mydata_str = JSON.stringify(data);
-  var mydata_str_sc = mydata_str.replace(/\\n/g, "\\n")
-                                      .replace(/\\'/g, "\\'")
-                                      .replace(/\\"/g, '\\"')
-                                      .replace(/\\&/g, "\\&")
-                                      .replace(/\\r/g, "\\r")
-                                      .replace(/\\t/g, "\\t")
-                                      .replace(/\\b/g, "\\b")
-                                      .replace(/\\f/g, "\\f");
-  console.error(mydata_str_sc[0]["Process Type"]);
-  console.error(mydata_str_sc[1]["Process Name"]);
+  var mydata_str = JSON.parse(data);
+  alert(mydata_str[0]["Process Type"]);
+  alert(mydata_str[1]["Process Name"]);
 }
