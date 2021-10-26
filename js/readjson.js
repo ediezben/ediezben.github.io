715c8eeb1e580fd7d5d8958df8ab2b1768ca2758
ediezben <ediezben@gmail.com>
2021-10-26 16:45:49 +0200

    Test5

--- js/readjson.js
+++ js/readjson.js
@@ -1,5 +1,13 @@
 function load() {
-  var mydata = JSON.parse(data);
-  alert(mydata[0]);
-  alert(mydata[1]);
+  var mydata_str = JSON.stringify(data);
+  var mydata_str_sc = mydata_str.replace(/\\n/g, "\\n")
+                                      .replace(/\\'/g, "\\'")
+                                      .replace(/\\"/g, '\\"')
+                                      .replace(/\\&/g, "\\&")
+                                      .replace(/\\r/g, "\\r")
+                                      .replace(/\\t/g, "\\t")
+                                      .replace(/\\b/g, "\\b")
+                                      .replace(/\\f/g, "\\f");
+  alert(mydata_str_sc[0]);
+  alert(mydata_str_sc[1]);
 }
