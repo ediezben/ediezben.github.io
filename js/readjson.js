73340269b01396d29f7e6b4b7d76386ed5dc51eb
ediezben <ediezben@gmail.com>
2021-10-26 16:26:13 +0200

    Fix readjson.js

--- js/readjson.js
+++ js/readjson.js
@@ -1,5 +1,7 @@
-var mydata = JSON.parse(data);
-alert(mydata[0].'Process Type');
-alert(mydata[0].'Process Name');
-alert(mydata[1].'Process Type');
-alert(mydata[1].'Process Name');
+function load() {
+  var mydata = JSON.parse(data);
+  alert(mydata[0].'Process Type');
+  alert(mydata[0].'Process Name');
+  alert(mydata[1].'Process Type');
+  alert(mydata[1].'Process Name');
+}
