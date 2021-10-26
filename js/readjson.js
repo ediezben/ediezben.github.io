ad3c24e7004520fc6d9f9bbdef8157266c1a1b58
ediezben <ediezben@gmail.com>
2021-10-26 18:05:09 +0200

    Test_table_format

--- js/readjson.js
+++ js/readjson.js
@@ -6,9 +6,11 @@
 
 function constructTable(selector) {
           var list = JSON.parse(data);
+          var keys = Object.keys(list);
           alert(list[0]["Process Type"]);
           alert(list[1]["Process Name"]);
           // Getting the all column names
+
           var cols = Headers(list, selector);
 
           // Traversing the JSON data
@@ -32,6 +34,11 @@ function constructTable(selector) {
       function Headers(list, selector) {
           var columns = [];
           var header = $('<tr/>');
+          var keys = keys.map( (value) => value["Process Type"]).filter( (value, index, _arr) => _arr.indexOf(value) == index);
+          for (var i = 0; i < keys.length; i++) {
+            var first_key = keys[i];
+            $(selector).append($('<tr data-toggle="collapse" data-target="#demo1" class="accordion-toggle"><td></td></tr>').html(first_key));
+          }
 
           for (var i = 0; i < list.length; i++) {
               var row = list[i];
