3a87ed40844ef6c308f48b4582231731c8afdb23
ediezben <ediezben@gmail.com>
2021-10-26 17:33:54 +0200

    Adding table

--- js/readjson.js
+++ js/readjson.js
@@ -1,50 +1,50 @@
 function load() {
   var mydata_str = JSON.parse(data);
-
-  function constructTable(selector) {
-
-            // Getting the all column names
-            var cols = Headers(list, selector);
-
-            // Traversing the JSON data
-            for (var i = 0; i < list.length; i++) {
-                var row = $('<tr/>');
-                for (var colIndex = 0; colIndex < cols.length; colIndex++)
-                {
-                    var val = list[i][cols[colIndex]];
-
-                    // If there is any key, which is matching
-                    // with the column name
-                    if (val == null) val = "";
-                        row.append($('<td/>').html(val));
-                }
-
-                // Adding each row to the table
-                $(selector).append(row);
-            }
-        }
-
-        function Headers(list, selector) {
-            var columns = [];
-            var header = $('<tr/>');
-
-            for (var i = 0; i < list.length; i++) {
-                var row = list[i];
-
-                for (var k in row) {
-                    if ($.inArray(k, columns) == -1) {
-                        columns.push(k);
-
-                        // Creating the header
-                        header.append($('<th/>').html(k));
-                    }
-                }
-            }
-
-            // Appending the header to the table
-            $(selector).append(header);
-                return columns;
-        }
   alert(mydata_str[0]["Process Type"]);
   alert(mydata_str[1]["Process Name"]);
 }
+
+function constructTable(selector) {
+          var list = JSON.parse(data);
+          // Getting the all column names
+          var cols = Headers(list, selector);
+
+          // Traversing the JSON data
+          for (var i = 0; i < list.length; i++) {
+              var row = $('<tr/>');
+              for (var colIndex = 0; colIndex < cols.length; colIndex++)
+              {
+                  var val = list[i][cols[colIndex]];
+
+                  // If there is any key, which is matching
+                  // with the column name
+                  if (val == null) val = "";
+                      row.append($('<td/>').html(val));
+              }
+
+              // Adding each row to the table
+              $(selector).append(row);
+          }
+      }
+
+      function Headers(list, selector) {
+          var columns = [];
+          var header = $('<tr/>');
+
+          for (var i = 0; i < list.length; i++) {
+              var row = list[i];
+
+              for (var k in row) {
+                  if ($.inArray(k, columns) == -1) {
+                      columns.push(k);
+
+                      // Creating the header
+                      header.append($('<th/>').html(k));
+                  }
+              }
+          }
+
+          // Appending the header to the table
+          $(selector).append(header);
+              return columns;
+      }
