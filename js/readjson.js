ad8ae910f3afd9a132f4cf4362616ace9128a3f2
ediezben <ediezben@gmail.com>
2021-10-31 17:25:46 +0100

    Fixed table collapsing and format first two groups

--- js/readjson.js
+++ js/readjson.js
@@ -7,8 +7,8 @@
 function constructTable(selector) {
           var list = JSON.parse(data);
           var keys = Object.keys(list);
-          alert(list[0]["Process Type"]);
-          alert(list[1]["Process Name"]);
+          // alert(list[0]["Process Type"]);
+          // alert(list[1]["Process Name"]);
           // Getting the all column names
 
           var cols = Headers(list, selector);
@@ -43,38 +43,50 @@ function constructTable(selector) {
 
       function Headers(list, selector) {
           var columns = [];
-          var header = $('<tr/>');
+          $(selector).append($('</thead>'));
+          var html_t = $('<tr>');
           var keys = list.map( (value) => value["Process Type"]).filter( (value, index, _arr) => _arr.indexOf(value) == index);
+          alert(keys);
           // Adding first rows for selecting Processes
+          html_t+='<tbody>';
           for (var i = 0; i < keys.length; i++) {
             var first_key = keys[i];
-            var key_id = first_key + "_col";
-            $(selector).append($('<tr data-toggle="collapse" data-target="#' + key_id +'" class="accordion-toggle"><td></td></tr>').html(first_key));
+            var key_id = "a"+ i + "_col";
+            html_t+='<tr data-toggle="collapse" data-target="#' + key_id +'" class="accordion-toggle"><td>'+first_key+'</td></tr>';
+            // $(selector).append($('<tr data-toggle="collapse" data-target="#' + key_id +'" class="accordion-toggle"><td></td></tr>').html(first_key));
             var obj_step_1 = searchJSON(list,"Process Type",first_key);
             var keys_step_1 = obj_step_1.map( (value) => value["Process Name"]).filter( (value, index, _arr) => _arr.indexOf(value) == index);
-            $(selector).append($('<tr><td colspan="12" class="hiddenRow"><div class="accordian-body collapse" id="'+key_id+'">'));
+            html_t+='<tr><td colspan="12" class="hiddenRow"><div class="accordian-body collapse" id="'+key_id+'">';
+            // $(selector).append($('<tr><td colspan="12" class="hiddenRow"><div class="accordian-body collapse" id="'+key_id+'">'));
+            alert(keys_step_1);
+            html_t+='<table class="table table-striped">';
             for (var k = 0; k < keys_step_1.length; k++) {
-              var second_key = keys_step_1[i];
-              var second_key_id = second_key + "_col";
-              $(selector).append($('<tr data-toggle="collapse" data-target="#' + second_key_id +'" class="accordion-toggle id="'+key_id+'"><td></td></tr>').html(second_key));
+              var second_key = keys_step_1[k];
+              var second_key_id = "b" + k + "_col";
+              html_t+='<tr data-toggle="collapse" data-target="#' + second_key_id +'" class="accordion-toggle id="'+key_id+'"><td>'+second_key+'</td></tr>';
+              // $(selector).append($('<tr data-toggle="collapse" data-target="#' + second_key_id +'" class="accordion-toggle id="'+key_id+'"><td></td></tr>').html(second_key));
             }
-            $(selector).append($('</tr></td></div>'));
+            html_t+='</table>';
+            html_t+='</tr></td></div>';
+            // $(selector).append($('</tr></td></div>'));
           }
+          html_t+='</tbody>';
 
-          for (var i = 0; i < list.length; i++) {
-              var row = list[i];
-
-              for (var k in row) {
-                  if ($.inArray(k, columns) == -1) {
-                      columns.push(k);
-
-                      // Creating the header
-                      header.append($('<th/>').html(k));
-                  }
-              }
-          }
+          // for (var i = 0; i < list.length; i++) {
+          //     var row = list[i];
+          //
+          //     for (var k in row) {
+          //         if ($.inArray(k, columns) == -1) {
+          //             columns.push(k);
+          //
+          //             // Creating the header
+          //             header.append($('<th/>').html(k));
+          //         }
+          //     }
+          // }
 
           // Appending the header to the table
+          $(selector).append(html_t);
           // $(selector).append(header);
               return columns;
       }
